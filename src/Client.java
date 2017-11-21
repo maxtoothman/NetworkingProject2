fb130808c32ee0999b2be1dd956b430fb29b9b2c
Nick Hutchinson <nickhutchinson@me.com>
2017-11-21 03:47:36 -0500

    v1.0
    Signed-off-by: Nick Hutchinson <nickhutchinson@me.com>

--- src/Client.java
+++ src/Client.java
@@ -1,7 +1,6 @@
-package src;
-
 import java.io.*;
 import java.net.Socket;
+import java.util.Objects;
 import java.util.Scanner;
 
 /**
@@ -22,32 +21,39 @@ import java.util.Scanner;
  *                             MESSAGE <text>
  *
  */
-public class HangmanClient {
 
-    private byte[] readBuffer;
+//Client class that contains the HangmanClient object
+public class Client {
+
+
+    private final byte[] readBuffer;
     private int numIncorrect;
-    int wordLength;
     private Socket socket;
     private InputStream stream;
     private DataOutputStream writer;
     private Scanner scan;
     private int msgFlag;
-    private String message;
-
+    private String guesses;
+    private String correctGuesses;
     private boolean runGame = false;
 
-    private HangmanClient() throws Exception{
+    // Client constructor
+    //Sets up the client object
+    private Client(String ip, int port) throws Exception{
         readBuffer = new byte[50];
         numIncorrect = 0;
-        socket = new Socket("localhost", 8080);
+        socket = new Socket(ip, port);
         stream = socket.getInputStream();
         writer = new DataOutputStream(socket.getOutputStream());
         //System.out.println("New Client Created");
         scan = new Scanner(System.in);
     }
 
-    private int endConnection() throws IOException {
+    //Terminates the client-side connection
+    private int endConnection() {
         try {
+            stream.close();
+            writer.close();
             socket.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -56,6 +62,7 @@ public class HangmanClient {
         return 0;
     }
 
+    //Main game loop
     private void playGame() throws Exception {
         initiateGame();
         while(runGame) {
@@ -64,15 +71,21 @@ public class HangmanClient {
         }
     }
 
-
+    //Sets up the game. Checks if the user wants to play the game
     private void initiateGame() throws Exception {
         System.out.printf("Ready to start the game? (y/n):");
-        String message = askUserInput();
+        String message = askUserInput().toLowerCase();
         //Make sure the start game message is "y". Otherwise exit application
-        runGame = (runGame || !message.equals("n")) && !runGame && message.equals("y");
+        while (!Objects.equals(message, "n")
+                && !Objects.equals(message,"y")) {
+            System.out.println("Please select either 'y' or 'n'.");
+            message = askUserInput().toLowerCase();
+        }
+        runGame = (runGame || !message.equals("n")) && !runGame
+                && message.equals("y");
 
-        //TODO: add a check to ask again if start game response is not "y/Y" or "n/N"
         if (!runGame) {
+            //noinspection ControlFlowStatementWithoutBraces,StatementWithEmptyBody
             while (endConnection() == 1);
             System.exit(0);
         } else {
@@ -80,41 +93,80 @@ public class HangmanClient {
         }
     }
 
+    //Asks the user to guess a letter and checks for proper input.
+    // Writes to the server
     private void guessLetter() throws Exception {
-        System.out.println("Next Letter To Guess: ");
-        String message = this.askUserInput();
+        System.out.println("Letter To Guess: ");
+        String message = this.askUserInput().toLowerCase();
+        boolean goodGuess = false;
+        while(!goodGuess) {
+            if (message.length() != 1) {
+                System.out.println("Please enter exactly 1 character.");
+                message = askUserInput().toLowerCase();
+            } else if (!Character.isLetter(message.charAt(0))) {
+                System.out.println("Please enter a character a-z.");
+                message = askUserInput().toLowerCase();
+            } else if (guesses.contains(message)) {
+                System.out.println("Please enter a character that you have " +
+                        "not yet guessed.");
+                message = askUserInput().toLowerCase();
+            } else if (correctGuesses.contains(message)) {
+                System.out.println("Please enter a character that you have " +
+                        "not yet guessed.");
+                message = askUserInput().toLowerCase();
+            } else {
+                goodGuess = true;
+            }
+        }
         this.writeMessage(message);
     }
 
+    //Read message helper function.
+    // Handles the output and formatting from the server
+    @SuppressWarnings("ResultOfMethodCallIgnored")
     private void checkMessage() throws IOException {
         if (msgFlag > 0) {
             this.stream.read(this.readBuffer);
-            message = new String(readBuffer).substring(0,
+            String message = new String(readBuffer).substring(0,
                     msgFlag);
             System.out.println(message);
             this.endConnection();
             System.exit(0);
         } else {
-            this.wordLength = this.stream.read();
+            int wordLength = this.stream.read();
             //System.out.println("Word Length is " + wordLength);
             this.numIncorrect = this.stream.read();
             //System.out.println("Number Incorrect is " + numIncorrect);
             this.stream.read(this.readBuffer);
             String message = new String(readBuffer);
-            String guesses = message.substring(wordLength,
+            guesses = message.substring(wordLength,
                     wordLength + numIncorrect);
-            message = message.substring(0,wordLength);
+            guesses = addSpaces(guesses);
+            message = message.substring(0, wordLength);
+            message = addSpaces(message);
+            correctGuesses = message;
             System.out.println(message);
-            System.out.println("Incorrect Guesses: " + guesses);
+            System.out.println("Incorrect Guesses: " + guesses + "\n");
         }
     }
 
+    //Helper method to add spaces to various Strings
+    private String addSpaces(String message) {
+        StringBuilder newString = new StringBuilder();
+        for (int i = 0; i < message.length(); i++) {
+            newString.append(message.charAt(i)).append(" ");
+        }
+        return newString.toString();
+    }
 
+    //Wrapper function to read messages from the server
     private void readMessage() throws Exception{
         msgFlag = this.stream.read();
         this.checkMessage();
     }
 
+    //Function to write messages to the server.
+    @SuppressWarnings("Duplicates")
     private void writeMessage(String message) throws Exception {
         byte msgLength = (byte) (message.length());
         byte[] messageBytes = message.getBytes();
@@ -125,14 +177,16 @@ public class HangmanClient {
         this.writer.write(byteMessage, 0, message.length() + 1);
     }
 
+    //Helper method to ask for the user's input
     private String askUserInput() {
         return this.scan.nextLine();
     }
 
+    //Main method.
     public static void main(String[] args) throws Exception {
         //System.out.println("Hangman Client is Running");
-        HangmanClient client = new HangmanClient();
-        //while loop to execute gameplay
+        Client client = new Client(args[0], Integer.parseInt(args[1]));
+        //while loop to execute game play
         client.playGame();
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,86 +1,133 @@
-package src;
-
 import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Queue;
 import java.util.Random;
-import java.util.Scanner;
 
+//Server object containing the server administrative duties.
+public class Server {
 
-public class HangmanServer {
 
     static String[] dictionary = {"banana", "mistake", "hopons", "her",
             "illusion", "marryme", "blueman", "bees", "cornball", "cousin",
             "family", "seal", "poppop", "freebie", "franklin"};
 
     private ServerSocket listener;
-    private boolean healthyConnection = true;
+    private final ArrayList<Game> games = new ArrayList<>();
 
-
-    private HangmanServer() throws Exception {
+    //Constructor requiring a port number
+    private Server(int port) throws Exception {
         listener = new ServerSocket();
-        InetSocketAddress listenerAddress = new InetSocketAddress(8080);
+        InetSocketAddress listenerAddress = new InetSocketAddress(port);
         listener.bind(listenerAddress);
     }
 
-    private void acceptIncomingConnetions() throws Exception {
+    //Function that accepts connections coming into the server.
+    private void acceptIncomingConnections() throws Exception {
         System.out.println("Hangman Server is Running");
-        while (healthyConnection) {
-            new Game(listener.accept()).start();
+        //noinspection InfiniteLoopStatement
+        while (true) {
+            Socket temp = listener.accept();
+            int i = games.size() - 1;
+            while (i > 0) {
+                if (games.get(i).getState() != Thread.State.RUNNABLE) {
+                    games.remove(i);
+                }
+                i--;
+            }
+            Game g = new Game(temp, games.size());
+            if (games.size() < 3) {
+                games.add(g);
+                g.start();
+            } else {
+                g.start();
+            }
         }
     }
 
-
-
-
+    //main method
     public static void main(String[] args) throws Exception {
-        HangmanServer server = new HangmanServer();
-        server.acceptIncomingConnetions();
+        if (args.length > 1) {
+            readTextFile(args[1]);
+        }
+        Server server = new Server(Integer.parseInt(args[0]));
+        server.acceptIncomingConnections();
     }
+
+    //Static helper method to read in text file.
+    private static void readTextFile(String file) throws IOException {
+        boolean firstPass = true;
+        int numWords;
+        int counter = 0;
+        for (String line : Files.readAllLines(Paths.get(file))) {
+            if (firstPass) {
+                String[] numbers = line.split(" ");
+                numWords = Integer.parseInt(numbers[numbers.length - 1]);
+                dictionary = new String[numWords];
+                firstPass = false;
+            } else {
+                dictionary[counter] = line;
+                counter++;
+            }
+        }
+     }
 }
 
+//Game object that contains all game logic
 class Game extends Thread {
 
-    private Socket clientSocket;
-    private byte[] readBuffer;
-    private String word;
-    private byte[] emptyWordArray;
-    private byte[] guessArray;
+    private final Socket clientSocket;
+    private final byte[] readBuffer;
+    private final String word;
+    private final byte[] emptyWordArray;
+    private final byte[] guessArray;
     private DataOutputStream writer;
     private int numIncorrect;
-    private int wordLength;
+    private final int wordLength;
     private InputStream stream;
     private boolean  runGame = true;
+    private final int gamesLength;
+    private int msgFlag;
 
-    Game(Socket clientSocket) throws Exception{
+    //Game constructor that requires a socket and the number of ongoing games
+    Game(Socket clientSocket, int gamesLength) {
         readBuffer = new byte[50];
         this.clientSocket = clientSocket;
         numIncorrect = 0;
-        word = HangmanServer.dictionary[new Random().nextInt(15)];
+        word = Server.dictionary[new Random().nextInt(15)];
         wordLength = word.length();
         guessArray = new byte[6];
         emptyWordArray = new byte[wordLength];
         for (int i =0;i<wordLength;i++) {
             emptyWordArray[i] = (byte)'_';
         }
-        //System.out.println("Empty word array is " + Arrays.toString
-                //(emptyWordArray));
-
-
+        this.gamesLength =  gamesLength;
     }
 
+    //Run method required to extend Thread
+    //Contains the connection logic
     public void run() {
         try {
+            System.out.println("Connection from "
+                    + clientSocket.getInetAddress() + ":"
+                    + clientSocket.getPort());
             writer = new DataOutputStream(clientSocket.getOutputStream());
             stream = clientSocket.getInputStream();
 
-            this.playGame();
+            if (gamesLength >= 3) {
+                writeMessage("Server overloaded.");
+            } else {
+                this.playGame();
+            }
 
             writer.close();
             stream.close();
+            System.out.println("Terminating connection "
+                    + clientSocket.getInetAddress()
+                    + ":" +clientSocket.getPort());
 
             clientSocket.close();
 
@@ -89,14 +136,12 @@ class Game extends Thread {
         }
     }
 
-    private String getWord() {
-        return this.word;
-    }
-
+    //Initializes the game
     private void startGame() throws Exception {
-        if (readMessage().length() == 0) {
+        readMessage();
+        if (msgFlag == 0) {
             System.out.println("Game Initiated");
-            System.out.println("Word is: " + this.getWord());
+            System.out.println("Word is: " + this.word);
             this.writeControl();
         } else {
             runGame = false;
@@ -104,14 +149,16 @@ class Game extends Thread {
 
     }
 
-    void playGame() throws Exception {
+    //The main game loop
+    private void playGame() throws Exception {
         this.startGame();
         while (runGame) {
             this.updateGame(this.readMessage());
             if (this.didLose()) {
-                this.writeMessage("You Lose");
+                this.writeMessage("The word was " + this.word + ".\nYou Lose!");
                 runGame = false;
             } else if (this.didWin()) {
+                this.writeMessage("The word was " + this.word + ".\nYou Win!");
                 this.writeMessage("You Win");
                 runGame = false;
             } else {
@@ -120,6 +167,7 @@ class Game extends Thread {
         }
     }
 
+    //Checks if the game was won
     private boolean didWin(){
         for (byte anEmptyWordArray : this.emptyWordArray) {
             if ((char) anEmptyWordArray == '_') {
@@ -129,10 +177,12 @@ class Game extends Thread {
         return true;
     }
 
+    //Checks if the game was lost
     private boolean didLose() {
         return this.numIncorrect >= 6;
     }
 
+    //Updates the game data based on a player's guess
     private void updateGame(String message) {
         for (int indexLocation :
                 this.findCharactersInWord(message.charAt(0))) {
@@ -149,16 +199,21 @@ class Game extends Thread {
         }
     }
 
+    //Finds all of the matching characters in the word
+    //returns a list of their indices
     private ArrayList<Integer> findCharactersInWord(char character) {
         ArrayList<Integer> charArray = new ArrayList<>();
         for (int i = 0; i < word.length(); i++) {
-            if (word.charAt(i) == character) {
+            if (Character.toLowerCase(word.charAt(i))
+                    == Character.toLowerCase(character)) {
                 charArray.add(i);
             }
         }
         return charArray;
     }
 
+    //Formats and writes a message to the client
+    @SuppressWarnings("Duplicates")
     private void writeMessage(String message) throws Exception{
         byte flag = (byte) (message.length());
         byte[] messageBytes = message.getBytes();
@@ -170,7 +225,8 @@ class Game extends Thread {
         this.writer.write(byteMessage,0,message.length()+1);
     }
 
-    void writeControl() throws Exception{
+    //Formats and writes a control message to the client
+    private void writeControl() throws Exception{
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         out.write((byte) 0);
         out.write((byte) wordLength);
@@ -182,14 +238,15 @@ class Game extends Thread {
         this.writer.write(controlMessage,0,wordLength+3+numIncorrect);
     }
 
-    String readMessage() throws Exception {
-        int msgFlag = this.stream.read();
+    //Reads and formats a message from the client
+    private String readMessage() throws Exception {
+        msgFlag = this.stream.read();
         //System.out.println(readBuffer);
         //System.out.println(msgFlag);
         String message;
         if (msgFlag > 0) {
+            //noinspection ResultOfMethodCallIgnored
             this.stream.read(this.readBuffer);
-//            System.out.println("Read message status: " + status);
             message = new String(readBuffer);
             return message.substring(0,msgFlag);
         }
